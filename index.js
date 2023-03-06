4e07d0887a7d76a473c870f6488851e5648028c0
Ignacio Javier Julio Posada <67277216+ignaciojulio@users.noreply.github.com>
2023-03-06 16:44:59 -0500

    update

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const http = require('http');
+const fs = require('fs');
+const port = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-    res.send('¡Hola, mundo!');
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST' && req.url === '/order') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      const orderNumber = JSON.parse(body).ordernumber;
+      const orders = JSON.parse(fs.readFileSync('orders.json'));
+      const order = orders.find(o => o.ordernumber === orderNumber);
+      let answer = '';
+      if (order) {
+        answer = {
+          answer: `${order.status}`
+        };
+      } else {
+        answer = {
+          answer: 'Order not found'
+        };
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(answer));
+    });
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
-app.post('/api/usuarios', (req, res) => {
-    console.log(req.body);
-    res.json({ message: 'Usuario creado correctamente' });
-});
-
-app.listen(port, () => {
-    console.log(`La aplicación está corriendo en el puerto ${port}`);
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
