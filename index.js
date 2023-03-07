924ca31f1f092c3d9fd06a259a60c0dc99bacfeb
Ignacio Javier Julio Posada <67277216+ignaciojulio@users.noreply.github.com>
2023-03-07 13:41:17 -0500

    fix: index update: package.json

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => res.type('html').send(html));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
 const http = require('http');
 const fs = require('fs');
+const cors = require('cors');
+const port = 3000;
 
 
 const server = http.createServer((req, res) => {
