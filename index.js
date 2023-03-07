4b35f4460ea9874845cf27e0d2067aae2eee098f
Ignacio Javier Julio Posada <67277216+ignaciojulio@users.noreply.github.com>
2023-03-07 11:05:10 -0500

    update

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/cita', (req, res) => {
+  const { cedula } = req.body;
+  const citasAsignadas = JSON.parse(fs.readFileSync('citas.json')).find(o => o.cedula === cedula)?.citasAsignadas;
+  const answer = citasAsignadas
+        ? { answer: `Usted tiene ${citasAsignadas.length} citas asignadas\n\n${citasAsignadas.join(", ")}` }
+        : { answer: `No se encontraron citas asignadas a la cedula ${cedula}` };
+  res.setHeader('Content-Type', 'application/json');
+  res.json(answer);
+});
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
 const port = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-    res.send('¡Hola, mundo!');
-});
-
-app.post('/api/usuarios', (req, res) => {
-    console.log(req.body);
-    res.json({ message: 'Usuario creado correctamente' });
-});
-
 app.listen(port, () => {
-    console.log(`La aplicación está corriendo en el puerto ${port}`);
+  console.log(`Server running on port ${port}`);
 });
