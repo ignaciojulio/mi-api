32eebeec72cff51c6a3bbfa90f32c7d3a5a349ae
Ignacio Javier Julio Posada <67277216+ignaciojulio@users.noreply.github.com>
2023-03-07 10:50:48 -0500

    update

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-/* Importación del módulo http. */
 const http = require('http');
-/* Importación del módulo del sistema de archivos. */
 const fs = require('fs');
-/* Declarando una variable constante llamada puerto y asignándole el valor 3000. */
+const cors = require('cors');
 const port = 3000;
 
-/* Crear un servidor que escuche las solicitudes POST en la ruta /cita. */
+
 const server = http.createServer((req, res) => {
-  /* Esta es una declaración condicional que verifica si el método de solicitud es POST y la URL de la
-  solicitud es /cita. Si ambas condiciones son verdaderas, entonces se lee el cuerpo de la solicitud
-  y se extrae la cédula del cuerpo. Luego, las citasAsignadas se extraen del archivo citas.json.
-  Finalmente, se envía la respuesta al cliente. */
   if (req.method === 'POST' && req.url === '/cita') {
     let body = '';
-    /* Escuchar eventos de datos. */
     req.on('data', chunk => {
       body += chunk.toString();
     });
-    /* Esta es una función de devolución de llamada que se ejecuta cuando se completa la solicitud. */
     req.on('end', () => {
       const { cedula } = JSON.parse(body);
       const citasAsignadas = JSON.parse(fs.readFileSync('citas.json')).find(o => o.cedula === cedula)?.citasAsignadas;
@@ -27,17 +19,13 @@ const server = http.createServer((req, res) => {
       res.setHeader('Content-Type', 'application/json');
       res.end(JSON.stringify(answer));
     });
-  }/* Esta es una declaración condicional que verifica si el método de solicitud es POST y la URL de la
-  solicitud es /cita. Si ambas condiciones son verdaderas, se lee el cuerpo de la solicitud y se
-  extrae el ID del cuerpo. Luego, las Citas asignadas se extraen del archivo citas.json.
-  Finalmente, se envía la respuesta al cliente. */
+  }
   else {
     res.statusCode = 404;
     res.end();
   }
 });
 
-/* Escuchando el puerto 3000. */
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
