215f643ce74d91e18d106d678f1ca61632acefe2
Ignacio Javier Julio Posada <67277216+ignaciojulio@users.noreply.github.com>
2023-03-07 13:05:41 -0500

    update

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-const express = require('express');
-const cors = require('cors');
-const fs = require('fs');
-const bodyParser = require('body-parser');
-
+const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(cors());
+app.get("/", (req, res) => res.type('html').send(html));
 
-app.post('/cita', (req, res) => {
-  const { cedula } = req.body;
-  const citasAsignadas = JSON.parse(fs.readFileSync('citas.json')).find(o => o.cedula === cedula)?.citasAsignadas;
-  const answer = citasAsignadas
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+const http = require('http');
+const fs = require('fs');
+
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST' && req.url === '/cita') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      const { cedula } = JSON.parse(body);
+      const citasAsignadas = JSON.parse(fs.readFileSync('citas.json')).find(o => o.cedula === cedula)?.citasAsignadas;
+      const answer = citasAsignadas
         ? { answer: `Usted tiene ${citasAsignadas.length} citas asignadas\n\n${citasAsignadas.join(", ")}` }
         : { answer: `No se encontraron citas asignadas a la cedula ${cedula}` };
-  res.setHeader('Content-Type', 'application/json');
-  res.json(answer);
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(answer));
+    });
+  }
+  else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
-app.use((req, res, next) => {
-  const error = new Error('Not found');
-  error.status = 404;
-  next(error);
-});
-
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message
-    }
-  });
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
