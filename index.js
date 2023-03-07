e17c7443eb49ccf1195d37cb7148b447f6b4cc1e
Ignacio Javier Julio Posada <67277216+ignaciojulio@users.noreply.github.com>
2023-03-07 09:37:37 -0500

    ecmascript 6 and Optimized version

--- index.js
+++ index.js
@@ -1,51 +1,43 @@
-/* Importación del módulo http (el servidor y el cliente HTTP) que se envía con Node.js. */
+/* Importación del módulo http. */
 const http = require('http');
 /* Importación del módulo del sistema de archivos. */
 const fs = require('fs');
 /* Declarando una variable constante llamada puerto y asignándole el valor 3000. */
 const port = 3000;
 
-/* Creando un servidor. */
+/* Crear un servidor que escuche las solicitudes POST en la ruta /cita. */
 const server = http.createServer((req, res) => {
-  /* El código anterior está creando un servidor que escucha un puerto y responde a una solicitud POST. */
+  /* Esta es una declaración condicional que verifica si el método de solicitud es POST y la URL de la
+  solicitud es /cita. Si ambas condiciones son verdaderas, entonces se lee el cuerpo de la solicitud
+  y se extrae la cédula del cuerpo. Luego, las citasAsignadas se extraen del archivo citas.json.
+  Finalmente, se envía la respuesta al cliente. */
   if (req.method === 'POST' && req.url === '/cita') {
-    /* Declarar una variable llamada cuerpo y asignarle una cadena vacía. */
     let body = '';
-    /* Escuchar datos y convertirlos en una cadena. */
+    /* Escuchar eventos de datos. */
     req.on('data', chunk => {
       body += chunk.toString();
     });
-    /* El código anterior lee el cuerpo de la solicitud y lo analiza en JSON. Luego está leyendo el
-    archivo citas.json y analizándolo a JSON. Luego es encontrar el objeto de cita que coincida con
-    la cédula en el cuerpo de la solicitud. Luego se va creando una cadena con el número de citas y
-    las propias citas. Luego está creando un objeto de respuesta con la cadena de respuesta. Luego,
-    establece el tipo de contenido en JSON y envía el objeto de respuesta como respuesta. */
+    /* Esta es una función de devolución de llamada que se ejecuta cuando se completa la solicitud. */
     req.on('end', () => {
-      const cedula = JSON.parse(body).cedula;
-      const citas = JSON.parse(fs.readFileSync('citas.json'));
-      const cita = citas.find(o => o.cedula === cedula);
-      let answer = '';
-      if (cita) {
-        const citas = cita.citasAsignadas.map(cita => cita + " \n\n").join(",");
-        answer = {
-          answer: `Usted tiene ${cita.citasAsignadas.length} citas asignadas \n\n,${citas}`
-        };
-      } else {
-        answer = {
-          answer: `No se encontro citas asignadas a la cedula ${cedula}`
-        };
-      }
+      const { cedula } = JSON.parse(body);
+      const citasAsignadas = JSON.parse(fs.readFileSync('citas.json')).find(o => o.cedula === cedula)?.citasAsignadas;
+      const answer = citasAsignadas
+        ? { answer: `Usted tiene ${citasAsignadas.length} citas asignadas\n\n${citasAsignadas.join(", ")}` }
+        : { answer: `No se encontraron citas asignadas a la cedula ${cedula}` };
       res.setHeader('Content-Type', 'application/json');
       res.end(JSON.stringify(answer));
     });
-    /* Establecer el código de estado en 404 y finalizar la respuesta. */
-  } else {
+  }/* Esta es una declaración condicional que verifica si el método de solicitud es POST y la URL de la
+  solicitud es /cita. Si ambas condiciones son verdaderas, se lee el cuerpo de la solicitud y se
+  extrae el ID del cuerpo. Luego, las Citas asignadas se extraen del archivo citas.json.
+  Finalmente, se envía la respuesta al cliente. */
+  else {
     res.statusCode = 404;
     res.end();
   }
 });
 
-/* Escuchando el puerto e imprimiendo un mensaje a la consola. */
+/* Escuchando el puerto 3000. */
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
